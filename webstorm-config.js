71dcfe6e20b5b88c07d00d15decb8c484fa5f0dc
Seb0g1 <144920708+Seb0g1@users.noreply.github.com>
2024-09-10 17:20:53 +0300

    Add files via upload

---
+++ webstorm-config.js
@@ -0,0 +1,16 @@
+System.config({
+  "paths": {
+    '#': path.resolve(__dirname, 'src/'),
+    '#comps': path.resolve(__dirname, '../../src/components'),
+    '#conts': path.resolve(__dirname, '../../src/containers'),
+    '#act': path.resolve(__dirname, '../../src/redux/actions'),
+    '#red': path.resolve(__dirname, '../../src/redux/reducers'),
+    '#store': path.resolve(__dirname, '../../src/redux/store.js'),
+    '#pages': path.resolve(__dirname, '../../src/pages'),
+    '#blocks': path.resolve(__dirname, '../../src/blocks'),
+    '#services': path.resolve(__dirname, '../../src/services'),
+    '#context': path.resolve(__dirname, '../../src/context'),
+    '#config': path.resolve(__dirname, '../../src/config'),
+    '#const': path.resolve(__dirname, '../../src/config/constants')
+  }
+});
